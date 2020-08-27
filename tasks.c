5d57e2088a5a63c9d9e16244b23e42d085eca3e2
Natalie Lim <natalielim@MacBook-Air.local>
2020-08-27 23:17:28 +1000

    Clean slate

--- tasks.c
+++ tasks.c
@@ -31,7 +31,6 @@ void max_min_flux(const char* flow_file)
     double x= 0.0, y = 0.0, u = 0.0, v = 0.0 , rho = 0.0;
     int num_total = 0;
     int num_lines = 0;
-    double task1array[][5];
     int n, m;
 
     FILE *fp_in = fopen(flow_file, "r"); //open file and read only
@@ -44,13 +43,7 @@ void max_min_flux(const char* flow_file)
     }
     // discard first line
     fscanf(fp_in, "%*[^\n]");
-    while(!feof(fp)) {
-      ch = fgetc(fp);
-      if(ch == '\n')
-        {
-        num_total++;
-        }
-      }
+
     /* while x, y , u, v, rho exists, and if x is larger than 20, then print to
     new file */
 
@@ -60,42 +53,8 @@ void max_min_flux(const char* flow_file)
           fprintf(fp_out, "%.6lf,%.6lf,%.6lf,%.6lf,%.6lf\n", x, y, u , v, rho);
         }
 
-        /* dynamically allocate matrix to be filled */
-        double **task1_array = (double**)safe_malloc(num_lines * sizeof(double *));
-        /*now allocate second dimension*/
-        for (i = 0; i < num_lines; i++) {
-          task1_array[i] = (double*)safe_malloc(TASK1COL * sizeof(double)); //2 asterix bc its pointer to pointer
-        }
 
 
-        for (i = 0; i < num_lines; i++){
-            for (j = 0; j < TASK1COL; j++) {
-              if (scanf("%.6lf", &(task1_array[i][j])) != 1) {
-                printf("\nFailed to read matrix data");
-                exit(EXIT_FAILURE);
-              }
-              printf("%.6lf ", task1_array[i][j]);
-            }
-            printf("\n");
-          }
-
-          //find min and max of each col
-          double *mins = (double *)malloc(TASK1COL * sizeof(double));
-          double *maxs = (double *)malloc(num_lines * sizeof(double));
-          for(i = 0; i < num_lines; i++) {
-            //top element fo each column is initial value
-            mins[j] = mat[0][j];
-            maxs[j] = mat[0][j];
-
-              for(i = 0; i < num_lines; i++) {
-                  if((mat[i][U]*mat[i][RHO])  < mins[j]) {
-                    mins[j] = mat[i][j];
-                  }
-                  if(mat[i][j] > maxs[j]) {
-                    maxs[j] = mat[i][j];
-                  }
-              }
-          }
 
       }
 
